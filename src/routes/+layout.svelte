34f08881cc4b0e60d45ae55b737b11ab42e8ac27
Lee Seung Jin <dltmdwls154@gmail.com>
2024-09-13 15:53:01 +0900

    Test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,10 @@
 	import Container from './Container.svelte';
 </script>
 
-<div class="app">
+<div class="bg-[gray] w-full h-full min-h-screen	">
+	<div class="text-column text-white text-[32px] font-bold">
+		<h1>Main Layout</h1>      
+	</div>
 	<Header />
 	<Container>
 		<slot />
---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+
+
+</script>
+
+<div class="bg-[gray] w-full h-full">
+
+<div class="text-column text-white text-[32px] font-bold">
+	<h1>About Layout</h1>
+		<slot />
+</div>
+
+</div>
+
